09f9af8ef3beef1fc030669e6d37ffdd92b1ac19
Eugene <gish.ee18@gmail.com>
2017-01-31 14:46:41 +0300

    Добавлены файлы kivy

---
+++ program.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from program import ChatBot
+
+class ChatBot(App):
+    def build(self):
+        pass
+
+    def on_pause(self):
+        return True
+
+    def on_resume(self):
+        pass
