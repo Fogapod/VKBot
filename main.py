7059c470e6cf828f52cc97020052a04b285c6b2e
fogapod <gish.ee18@gmail.com>
2017-02-01 21:29:33 +0300

    Убрана возможность отладки команд бота оффлайн

--- main.py
+++ main.py
@@ -15,14 +15,5 @@ def main():
         open(PATH + 'error.log', 'w').write(error_text)
         print(error_text)
 
-def bot_debug():
-    bot = Bot()
-    while True:
-        command = raw_input('command: ').lower()
-        args = ('command ' + raw_input('args: ')).split(' ')
-        try: print eval('bot.' + command + '(' + str(args) + ')')
-        except Exception as e: print 'error! ' + str(e)
-
 if __name__ == '__main__':
     main()
-    #bot_debug()
