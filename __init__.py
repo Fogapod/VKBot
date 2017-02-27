cff45364e3bdb1795b6152abe8e6f906bf0e1931
fogapod <gish.ee18@gmail.com>
2017-02-27 10:28:28 +0300

    Изменён путь к файлу токена для платформы андроид

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 __version__ = '0.1.0'
 __author__ = 'Eugene Ershov - https://vk.com/fogapod'
 
-from kivy.utils import platform
-
-PATH = '/storage/emulated/0/VKBot/' if platform == 'android' else ''
+PATH = ''
 DATA_PATH = 'data/'
