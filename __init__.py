735fd4ec51446bd12de05dab8a60d26ecef590cd
Eugene <gish.ee18@gmail.com>
2017-02-27 20:11:27 +0300

    Новый путь вызывает падение приложения
    . Вернул старый

--- __init__.py
+++ __init__.py
@@ -2,5 +2,7 @@ __version__ = '0.1.0'
 __author_vk_id__ = 180850898
 __author__ = 'Eugene Ershov - https://vk.com/id%d' % __author_vk_id__
 
-PATH = ''
+from kivy import platform
+
+PATH = '/sdcard/VKBot/' if platform == 'android' else ''
 DATA_PATH = 'data/'
