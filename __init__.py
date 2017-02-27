5fda9a558267e0f2f36d20bd0b0fde713f41d808
fogapod <gish.ee18@gmail.com>
2017-02-27 19:12:27 +0300

    Изменена ссылка на автора

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 __version__ = '0.1.0'
-__author__ = 'Eugene Ershov - https://vk.com/fogapod'
+__author_vk_id__ = 180850898
+__author__ = 'Eugene Ershov - https://vk.com/id%d' % __author_vk_id__
 
 PATH = ''
 DATA_PATH = 'data/'
