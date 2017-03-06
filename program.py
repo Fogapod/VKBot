88761ab2a3a48de6bb9900793c649b9ec46e51c7
fogapod <gish.ee18@gmail.com>
2017-03-06 09:38:09 +0300

    Добавлены уведомления о статусе бота, улучшено взаимодействие функций

--- program.py
+++ program.py
@@ -8,6 +8,7 @@ from kivy.clock import Clock
 
 from bot_core import LongPollSession
 
+from plyer import notification
 import androidhelper as sl4a
 droid = sl4a.Android()
 
@@ -77,7 +78,7 @@ class ChatBot(App):
 
 class LoginScreen(Screen):
 	def log_in(self):
-		droid.dialogCreateSpinnerProgress('Авторизация. Пожалуйста, подождите.', 'Это может занять несколько секунд')
+		droid.dialogCreateSpinnerProgress('Авторизация. Пожалуйста, подождите', 'Это может занять несколько секунд')
 		droid.dialogShow()
 
 		login = self.ids.login.text
@@ -86,9 +87,10 @@ class LoginScreen(Screen):
 		if login and password:
 			if session.authorization(login=login, password=password):
 				self.parent.show_home_form()
-				self.ids.pass_input.text = ''
+			else:
+				droid.makeToast('Неверный логин или пароль')
 
-		self.ids.login.text = ''
+		self.ids.pass_input.text = ''
 		droid.dialogDismiss()
 
 
@@ -96,36 +98,44 @@ class HomeScreen(Screen):
 	def __init__(self, *args, **kwargs):
 		super(HomeScreen, self).__init__(*args, **kwargs)
 		self.bot_check_event = Clock.schedule_interval(self.check_if_bot_active, 1)
-		
-	def on_main_btn_press(self):
-		run_bot_text = 'Запустить бота'
-		stop_bot_text = 'Остановить бота'
+		self.run_bot_text = 'Включить бота'
+		self.stop_bot_text = 'Выключить бота'
+		self.ids.main_btn.text = self.run_bot_text
 
+	def on_main_btn_press(self):
 		config = ChatBot.get_running_app().config
 
-		if self.parent.current_screen.ids.button.text == run_bot_text:
-			self.activation_status = config.getdefault('General', 'bot_activated', 'False')
-			use_custom_commands = config.getdefault('General', 'custom_commands', 'False')
-
-			while not session.start_bot(activated=self.activation_status == 'True',\
-				use_custom_commands=use_custom_commands == 'True'): continue
-
-			self.ids.button.text = stop_bot_text
-			self.bot_check_event()
+		if self.parent.current_screen.ids.main_btn.text == self.run_bot_text:
+			self.start_bot(config)
 		else:
-			self.bot_check_event.cancel()
-			bot_stopped = False
-
-			while not bot_stopped:
-				bot_stopped, new_activation_status = session.stop_bot()
-
-			if new_activation_status != self.activation_status:
-				config.set('General', 'bot_activated', str(new_activation_status))
-				config.write()
-
-			self.ids.button.text = run_bot_text
+			self.stop_bot(config)
 
 		#self.update_answers_count()
+
+	def start_bot(self, config):
+		self.activation_status = config.getdefault('General', 'bot_activated', 'False')
+		use_custom_commands = config.getdefault('General', 'custom_commands', 'False')
+
+		while not session.start_bot(activated=self.activation_status == 'True',\
+			use_custom_commands=use_custom_commands == 'True'): continue
+
+		self.ids.main_btn.text = self.stop_bot_text
+		self.bot_check_event()
+		notification.notify(title='VKBot',message='Bot active')
+
+	def stop_bot(self, config):
+		self.bot_check_event.cancel()
+		bot_stopped = False
+
+		while not bot_stopped:
+			bot_stopped, new_activation_status = session.stop_bot()
+
+		if new_activation_status != self.activation_status:
+			config.set('General', 'bot_activated', str(new_activation_status))
+			config.write()
+
+		self.ids.main_btn.text = self.run_bot_text
+		notification.notify(title='VKBot',message='Bot stopped')
 
 	def update_answers_count(self):
 		self.ids.answers_count_lb.text = 'Ответов: {}'.format(session.reply_count)
@@ -136,7 +146,7 @@ class HomeScreen(Screen):
 	
 	def check_if_bot_active(self, _):
 		if not session.running:
-			self.ids.button.text = 'Запустить бота'
+			self.ids.main_btn.text = self.run_bot_text
 			self.bot_check_event.cancel()
 
 class Root(ScreenManager):
