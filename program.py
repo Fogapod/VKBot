7329831762037cb2b42577e3d485f28182acaf3c
Eugene <gish.ee18@gmail.com>
2017-03-08 15:01:18 +0300

    Отступы заменены пробелами, поменял местами положение некоторых функций

--- program.py
+++ program.py
@@ -8,7 +8,6 @@ from kivy.clock import Clock
 
 from bot_core import LongPollSession
 
-
 #from plyer import notification
 
 #import android as sl4a
@@ -17,174 +16,174 @@ from bot_core import LongPollSession
 Builder.load_string('''
 #:import FadeTransition kivy.uix.screenmanager.FadeTransition
 <Root>:
-	id: rootscr
-	transition: FadeTransition()
+    id: rootscr
+    transition: FadeTransition()
 ''')
 
 
 class ChatBot(App):
-	use_kivy_settings = False
-	def __init__(self, *args, **kwargs):
-		super(ChatBot, self).__init__(*args, **kwargs)
-		self.root = Root()
-	
-	def on_stop(self):
-		while not session.stop_bot(): continue
-		#notification.notify(title='VKBot',message='Bot stopped')
+    use_kivy_settings = False
+    def __init__(self, *args, **kwargs):
+        super(ChatBot, self).__init__(*args, **kwargs)
+        self.root = Root()
+    
+    def build(self):
+        self.root.add_widget(HomeScreen())
+        self.root.add_widget(TwoFAKeyEnterForm())
+        self.root.add_widget(LoginScreen())
+        
+        activation_status = self.config.getdefault('General', 'bot_activated', 'False')
+        global session
+        session = LongPollSession(activated=activation_status == 'True')
 
-	def build(self):
-		self.root.add_widget(HomeScreen())
-		self.root.add_widget(TwoFAKeyEnterForm())
-		self.root.add_widget(LoginScreen())
-		
-		activation_status = self.config.getdefault('General', 'bot_activated', 'False')
-		global session
-		session = LongPollSession(activated=activation_status == 'True')
+        if not session.authorization()[0]:
+            self.root.show_auth_form()
 
-		if not session.authorization()[0]:
-			self.root.show_auth_form()
+        return self.root
 
-		return self.root
 
-	def on_pause(self):
-		return True
-		
-	def build_config(self, config):
-		config.setdefaults('General', 
-				{'show_bot_activity':'False', "bot_activated":'False', 'custom_commands':'False'}
-			)
+    def build_config(self, config):
+        config.setdefaults('General', 
+                {'show_bot_activity':'False', "bot_activated":'False', 'custom_commands':'False'}
+            )
 
-	def build_settings(self, settings):
-		settings.add_json_panel("Настройки бота", self.config, data=
-			'''[
-				{"type": "bool",
-				"title": "Отображать состояние бота в статусе",
-				"section": "General",
-				"key": "show_bot_activity",
-				"values": ["False","True"],
-				"disabled": 1
-				},
-				{"type": "bool",
-				"title": "Использоаать пользовательские команды (WIP)",
-				"section": "General",
-				"key": "custom_commands",
-				"values": ["False","True"]
-				},
-				{"type": "bool",
-				"title": "Бот активирован",
-				"section": "General",
-				"key": "bot_activated",
-				"values": ["False","True"],
-				"disabled": 1
-				}
-			]'''
-		)
+    def build_settings(self, settings):
+        settings.add_json_panel("Настройки бота", self.config, data=
+            '''[
+                {"type": "bool",
+                "title": "Отображать состояние бота в статусе",
+                "section": "General",
+                "key": "show_bot_activity",
+                "values": ["False","True"],
+                "disabled": 1
+                },
+                {"type": "bool",
+                "title": "Использоаать пользовательские команды (WIP)",
+                "section": "General",
+                "key": "custom_commands",
+                "values": ["False","True"]
+                },
+                {"type": "bool",
+                "title": "Бот активирован",
+                "section": "General",
+                "key": "bot_activated",
+                "values": ["False","True"],
+                "disabled": 1
+                }
+            ]'''
+        )
+
+    def on_pause(self):
+        return True
+
+    def on_stop(self):
+        while not session.stop_bot(): continue
+        #notification.notify(title='VKBot',message='Bot stopped')
 
 class LoginScreen(Screen):
-	def log_in(self, twofa_key=''):
-		login = self.ids.login.text
-		password = self.ids.pass_input.text
+    def log_in(self, twofa_key=''):
+        login = self.ids.login.text
+        password = self.ids.pass_input.text
 
-		if login and password:
-			#droid.dialogCreateSpinnerProgress('Авторизация. Пожалуйста, подождите', 'Это может занять несколько секунд')
-			#droid.dialogShow()
-			authorized, error = session.authorization(login=login, password=password, key=twofa_key)
-			if authorized:
-				if twofa_key:
-					self.ids.pass_input.text = ''
-					return True
-				else:
-					self.parent.show_home_form()
-			elif error:
-			    if error == 'Auth check code is needed':
-			        self.parent.show_twofa_form()
-			        return
-			#else:
-				#droid.makeToast('Неверный логин или пароль')
+        if login and password:
+            #droid.dialogCreateSpinnerProgress('Авторизация. Пожалуйста, подождите', 'Это может занять несколько секунд')
+            #droid.dialogShow()
+            authorized, error = session.authorization(login=login, password=password, key=twofa_key)
+            if authorized:
+                if twofa_key:
+                    self.ids.pass_input.text = ''
+                    return True
+                else:
+                    self.parent.show_home_form()
+            elif error:
+                if error == 'Auth check code is needed':
+                    self.parent.show_twofa_form()
+                    return
+            #else:
+                #droid.makeToast('Неверный логин или пароль')
 
-			#droid.dialogDismiss()
+            #droid.dialogDismiss()
 
-		self.ids.pass_input.text = ''
-		#return self.parent.current_screen == 'home_screen'
-		
+        self.ids.pass_input.text = ''
+        
 
 class TwoFAKeyEnterForm(Screen):
-	def twofa_auth(self):
-		if self.ids.twofa_textinput.text:
-			self.parent.show_auth_form(twofa_key=self.ids.twofa_textinput.text)
-			self.ids.twofa_textinput.text = ''
+    def twofa_auth(self):
+        if self.ids.twofa_textinput.text:
+            self.parent.show_auth_form(twofa_key=self.ids.twofa_textinput.text)
+            self.ids.twofa_textinput.text = ''
 
 class HomeScreen(Screen):
-	def __init__(self, *args, **kwargs):
-		super(HomeScreen, self).__init__(*args, **kwargs)
-		self.bot_check_event = Clock.schedule_interval(self.check_if_bot_active, 1)
-		self.run_bot_text = 'Включить бота'
-		self.stop_bot_text = 'Выключить бота'
-		self.ids.main_btn.text = self.run_bot_text
+    def __init__(self, *args, **kwargs):
+        super(HomeScreen, self).__init__(*args, **kwargs)
+        self.bot_check_event = Clock.schedule_interval(self.check_if_bot_active, 1)
+        self.run_bot_text = 'Включить бота'
+        self.stop_bot_text = 'Выключить бота'
+        self.ids.main_btn.text = self.run_bot_text
 
-	def on_main_btn_press(self):
-		config = ChatBot.get_running_app().config
+    def on_main_btn_press(self):
+        config = ChatBot.get_running_app().config
 
-		if self.parent.current_screen.ids.main_btn.text == self.run_bot_text:
-			self.start_bot(config)
-		else:
-			self.stop_bot(config)
+        if self.parent.current_screen.ids.main_btn.text == self.run_bot_text:
+            self.start_bot(config)
+        else:
+            self.stop_bot(config)
 
-	def start_bot(self, config):
-		self.activation_status = config.getdefault('General', 'bot_activated', 'False')
-		use_custom_commands = config.getdefault('General', 'custom_commands', 'False')
+    def start_bot(self, config):
+        self.activation_status = config.getdefault('General', 'bot_activated', 'False')
+        use_custom_commands = config.getdefault('General', 'custom_commands', 'False')
 
-		while not session.start_bot(activated=self.activation_status == 'True',\
-			use_custom_commands=use_custom_commands == 'True'): continue
+        while not session.start_bot(activated=self.activation_status == 'True',\
+            use_custom_commands=use_custom_commands == 'True'): continue
 
-		self.ids.main_btn.text = self.stop_bot_text
-		self.bot_check_event()
-		#notification.notify(title='VKBot',message='Bot active')
+        self.ids.main_btn.text = self.stop_bot_text
+        self.bot_check_event()
+        #notification.notify(title='VKBot',message='Bot active')
 
-	def stop_bot(self, config):
-		self.bot_check_event.cancel()
-		bot_stopped = False
+    def stop_bot(self, config):
+        self.bot_check_event.cancel()
+        bot_stopped = False
 
-		while not bot_stopped:
-			bot_stopped, new_activation_status = session.stop_bot()
+        while not bot_stopped:
+            bot_stopped, new_activation_status = session.stop_bot()
 
-		if new_activation_status != self.activation_status:
-			config.set('General', 'bot_activated', str(new_activation_status))
-			config.write()
+        if new_activation_status != self.activation_status:
+            config.set('General', 'bot_activated', str(new_activation_status))
+            config.write()
 
-		self.ids.main_btn.text = self.run_bot_text
-		#notification.notify(title='VKBot',message='Bot stopped')
+        self.ids.main_btn.text = self.run_bot_text
+        #notification.notify(title='VKBot',message='Bot stopped')
 
-	def update_answers_count(self):
-		self.ids.answers_count_lb.text = 'Ответов: {}'.format(session.reply_count)
+    def update_answers_count(self):
+        self.ids.answers_count_lb.text = 'Ответов: {}'.format(session.reply_count)
 
-	def logout(self):
-		session.authorization(logout=True)
-		self.parent.show_auth_form()
-	
-	def check_if_bot_active(self, _):
-		self.update_answers_count()
-		if not session.running:
-			self.ids.main_btn.text = self.run_bot_text
-			#notification.notify(title='VKBot',message='Bot stopped')
-			self.bot_check_event.cancel()
+    def logout(self):
+        session.authorization(logout=True)
+        self.parent.show_auth_form()
+    
+    def check_if_bot_active(self, _):
+        self.update_answers_count()
+        if not session.running:
+            self.ids.main_btn.text = self.run_bot_text
+            #notification.notify(title='VKBot',message='Bot stopped')
+            self.bot_check_event.cancel()
 
 
 class Root(ScreenManager):
-	def show_auth_form(self, twofa_key=''):
-		self.current = 'login_screen'
-		if twofa_key:
-			if self.current_screen.log_in(twofa_key=twofa_key):
-				self.show_home_form()
-				return
-		self.current_screen.ids.pass_auth.disabled = not session.authorized
-		
-	def show_home_form(self):
-		self.current = 'home_screen'
+    def show_auth_form(self, twofa_key=''):
+        self.current = 'login_screen'
+        if twofa_key:
+            if self.current_screen.log_in(twofa_key=twofa_key):
+                self.show_home_form()
+                return
+        self.current_screen.ids.pass_auth.disabled = not session.authorized
+        
+    def show_home_form(self):
+        self.current = 'home_screen'
 
-	def show_twofa_form(self):
-		self.current = 'twofa_form'
+    def show_twofa_form(self):
+        self.current = 'twofa_form'
 
 
 if __name__ == '__main__':
-	ChatBot().run()
+    ChatBot().run()
