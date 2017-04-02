3726520beb048ed619d3399c09904f3e31481380
Eugene <gish.ee18@gmail.com>
2017-04-02 20:20:23 +0300

    Убраны значения on_release (они назначаются при объявлении блоков)

--- uix/customcommandblock.py
+++ uix/customcommandblock.py
@@ -4,5 +4,9 @@ from kivy.uix.gridlayout import GridLayout
 class CustomCommandBlock(GridLayout):
     def __init__(self, **kwargs):
         super(CustomCommandBlock, self).__init__(**kwargs)
-        command = kwargs.get('command')
-        self.id, self.ids.command_btn.text = command, command
+        self.is_custom_command_block = True
+
+        self.command = kwargs['command']
+        self.response = kwargs['response']
+
+        self.ids.command_btn.text = self.command
