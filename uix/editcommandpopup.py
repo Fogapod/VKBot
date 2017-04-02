8353831cf42908db2bb03573ef15dc5de1396259
Eugene <gish.ee18@gmail.com>
2017-04-02 20:21:13 +0300

    Изменены получаемые значения

--- uix/editcommandpopup.py
+++ uix/editcommandpopup.py
@@ -6,4 +6,7 @@ class EditCommandPopup(Popup):
         super(EditCommandPopup, self).__init__(**kwargs)
         self.ids.command_text.text = kwargs.get('command_text', '')
         self.ids.response_text.text = kwargs.get('response_text', '')
-        self.item = kwargs['item']
+
+        self.list_item = kwargs['list_item']
+        if not self.list_item:
+        	self.ids.delete_command_btn.disabled = True
