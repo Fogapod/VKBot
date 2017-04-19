2975e3e54f7b2cb41f618b77ede0ee1ccde704f8
fogapod <gish.ee18@gmail.com>
2017-04-19 10:58:12 +0300

    Убран лишний параметр

--- uix/customcommandblock.py
+++ uix/customcommandblock.py
@@ -4,7 +4,6 @@ from kivy.uix.gridlayout import GridLayout
 class CustomCommandBlock(GridLayout):
     def __init__(self, **kwargs):
         super(CustomCommandBlock, self).__init__(**kwargs)
-        self.is_custom_command_block = True
         self.commands = []
         self.responses = []
 
