bce8d22ad06d93a626747325b76adeb35512e262
fogapod <gish.ee18@gmail.com>
2017-04-20 16:14:32 +0300

    Удалено повторное назначение заголовка всплывающего окна

--- uix/editcommandpopup.py
+++ uix/editcommandpopup.py
@@ -7,7 +7,6 @@ from kivy.uix.popup import Popup
 class EditCommandPopup(Popup):
     def __init__(self, **kwargs):
         super(EditCommandPopup, self).__init__(**kwargs)
-        self.title = kwargs['title']
         self.ids.command_text.text = kwargs['command_text']
         self.ids.response_text.text = kwargs['response_text']
         self.command_block = kwargs['command_block']
