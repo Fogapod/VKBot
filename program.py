c3447169749e7f6c334b68e816309486fcf80304
fogapod <gish.ee18@gmail.com>
2017-04-27 09:23:05 +0300

    Путь к файлу конфига указан напрямую

--- program.py
+++ program.py
@@ -49,8 +49,7 @@ class VKBotApp(App):
                     continue
 
     def get_application_config(self):
-        return super(VKBotApp, self).get_application_config(
-            '{}.%(appname)s.ini'.format(PATH))
+        return '{}.vkbot.ini'.format(PATH)
 
     def build_config(self, config):
         config.setdefaults('General', 
