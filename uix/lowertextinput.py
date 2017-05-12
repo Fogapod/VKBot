53a32fa41ae6f3cad5d0969f66a61b1bcb57ad68
fogapod <gish.ee18@gmail.com>
2017-05-12 22:59:07 +0300

    Изменён шрифт

--- uix/lowertextinput.py
+++ uix/lowertextinput.py
@@ -2,6 +2,10 @@ from kivy.uix.textinput import TextInput
 
 
 class LowerTextInput(TextInput):
+    def __init__(self, **kwargs):
+        self.font_name = 'Roboto-BoldItalic'
+        super(LowerTextInput, self).__init__(**kwargs)
+
     def insert_text(self, substring, from_undo=False):
         s = substring.lower()
         return super(LowerTextInput, self).insert_text(s, from_undo=False)
