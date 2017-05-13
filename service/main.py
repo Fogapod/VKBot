d68b31e1c11edb2e491fe7557b7ff731d138b01a
fogapod <gish.ee18@gmail.com>
2017-05-13 13:06:27 +0300

    Уменьшено время запуска бота

--- service/main.py
+++ service/main.py
@@ -8,6 +8,9 @@ from kivy import platform
 from kivy.logger import Logger
 from kivy.lib import osc
 
+import logging
+logging.captureWarnings(True)
+
 parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 parent_path += '\\' if platform == 'win' else '/'
 os.sys.path.append(parent_path)
@@ -39,11 +42,9 @@ def send_answers_count():
     osc.sendMsg('/answers', [session.reply_count, ], port=3002)
 
 def set_launch_params(message, *args):
-    global session, got_launch_params, activated
-    got_launch_params = True
+    global launch_params, activated
     launch_params = eval(message[2])
     activated = launch_params['activated']
-    session.load_params(**launch_params)
 
 def exit(*args):
     send_status('exiting')
@@ -52,28 +53,31 @@ def exit(*args):
 
 
 if __name__ == '__main__':
-    got_launch_params = False
-    activated = False 
+    activated = False
+    launch_params = {}
     osc.init()
     oscid = osc.listen(ipAddr='0.0.0.0', port=3000)
+    osc.bind(oscid, set_launch_params, '/launch_params')
+    send_status('connected')
+
     osc.bind(oscid, ping, '/ping')
-    osc.bind(oscid, set_launch_params, '/launch_params')    
-    osc.bind(oscid, exit, '/exit')    
+    osc.bind(oscid, exit, '/exit')
 
     session = LongPollSession()
-    send_status('connected')
     authorized, error = session.authorization()
 
     if error:
         send_error(error)
         exit()
     
-    while not got_launch_params:
+    while not launch_params:
         osc.readQueue(oscid)
         sleep(0.1)
 
     send_status('got params')
+    session.load_params(**launch_params)
     session.launch_bot()
+
     while True:
         osc.readQueue(oscid)
         send_answers_count()
