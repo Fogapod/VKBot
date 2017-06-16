8d558a63453ee9cc01b108c8d3528eaaea1a3014
fogapod <gish.ee18@gmail.com>
2017-06-16 13:48:36 +0300

    Версия 0.1.0dev

--- bot/core.py
+++ bot/core.py
@@ -14,7 +14,7 @@ CUSTOM_COMMAND_OPTIONS_COUNT
 
 import vkrequests as vkr
 
-__version__ = '0.0.9'
+__version__ = '0.1.0dev'
 AUTHOR_VK_ID = 180850898
 __author__ = 'Eugene Ershov - https://vk.com/id%d' % AUTHOR_VK_ID
 
