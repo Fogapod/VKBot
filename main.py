99c6f39f2be963efcc6bf6f7598adf3859722161
fogapod <gish.ee18@gmail.com>
2017-09-04 12:57:53 +0300

    Better import syntax

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import traceback
 import os
 
 from program import VKBotApp
-from bot.utils import PATH, DATA_PATH, save_error, ERROR_FILE_PATH
+from bot import utils
 
 
 def main():
     app = None
 
     try:
-        if not os.path.exists(PATH) and PATH:
-            os.makedirs(PATH)
-        if not os.path.exists(DATA_PATH):
-            os.makedirs(DATA_PATH)
+        if not os.path.exists(utils.PATH) and utils.PATH:
+            os.makedirs(utils.PATH)
+        if not os.path.exists(utils.DATA_PATH):
+            os.makedirs(utils.DATA_PATH)
 
         app = VKBotApp()
         app.run()
     except Exception:
         error_text = traceback.format_exc()
-        save_error(error_text)
+        utils.save_error(error_text)
 
         if app:
             try:
@@ -32,7 +32,7 @@ def main():
         from kivy.base import runTouchApp
         from uix.screens.exceptionscreen import ExceptionScreen
 
-        error_text += u'\nОшибка записана в файл %s' % ERROR_FILE_PATH
+        error_text += u'\nОшибка записана в файл %s' % utils.ERROR_FILE_PATH
         runTouchApp(ExceptionScreen(exception_text=error_text))
 
 if __name__ == '__main__':
