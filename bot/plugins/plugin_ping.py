ded215a7c88dbd691b8a9b7ee4d291ee2fd0e127
fogapod <gish.ee18@gmail.com>
2018-01-16 23:39:42 +0300

    Ping will now show how much time did it take to recieve message

--- bot/plugins/plugin_ping.py
+++ bot/plugins/plugin_ping.py
@@ -1,8 +1,10 @@
 #coding:utf8
 
+import time
+
 
 class Plugin(object):
-    __doc__ = '''Плагин предназначен для проверки активности бота.
+    __doc__ = '''Плагин предназначен для проверки времени ответа бота.
     Для использования необходимо иметь уровень доступа {protection} или выше
     Ключевые слова: [{keywords}]
     Использование: {keyword}
@@ -14,7 +16,8 @@ class Plugin(object):
     argument_required = False
 
     def respond(self, msg, rsp, *args, **kwargs):
+        delta = int(round((time.time() - msg.date) * 1000))
         rsp.forward_id = 0
-        rsp.text = 'pong'
+        rsp.text = u'pong, заняло ' + str(delta) + u'мс'
 
         return rsp
